1829ab26cbacd96346e2b93a28a7e4bd22d3e21f
AlbertoScalia <albscalia@gmail.com>
2023-04-18 15:43:42 +0200

    Feat (Javascript): Add Javascript with array

---
+++ Js/script.js
@@ -0,0 +1,10 @@
+//Creo un array contenente le varie mail
+const approvedEmails = [
+    "albescalia@gmail.com",
+    "albscalia@gmail.com",
+    "albertscalia@gmail.com",
+    "albertoscalia@gmail.com",
+    "albertoscl@gmail.com",
+    "alboscl@gmail.com",
+  ]
+  
