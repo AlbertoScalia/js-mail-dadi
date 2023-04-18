727f436ad9addac10fe0f7bb12658ff7d96b86f9
AlbertoScalia <albscalia@gmail.com>
2023-04-18 16:02:01 +0200

    Feat (Javascript) : Add addEventListener, For,  if

--- Js/script.js
+++ Js/script.js
@@ -6,5 +6,36 @@ const approvedEmails = [
     "albertoscalia@gmail.com",
     "albertoscl@gmail.com",
     "alboscl@gmail.com",
-  ]
-  
+]
+
+//Aggiungo le costanti
+const userEmail = document.getElementById("email");
+const btn = document.getElementById("send");
+const answer = document.getElementById("answer");
+
+//Creo un primo addEvent
+btn.addEventListener("click", function () {
+
+    let access = false;
+    answer.classList.remove("text-success", "text-danger");
+
+    // Creo un ciclo for 
+    for (let i = 0; i < approvedEmails.length && !access; i++) {
+
+        if ((userEmail.value) === approvedEmails[i]) access = true;
+    }
+
+    //Pongo le condizioni che aggiungano/tolgano delle classi bootstrap e i relativi messaggi
+    if (access) {
+        answer.classList.remove("d-none");
+        answer.classList.add("text-success", "fw-semibold");
+        message = "Accesso autorizzato!";
+
+    } else {
+        answer.classList.remove("d-none");
+        answer.classList.add("text-danger", "fw-semibold");
+        message = "Accesso negato!";
+    }
+
+    answer.innerHTML = message;
+})
